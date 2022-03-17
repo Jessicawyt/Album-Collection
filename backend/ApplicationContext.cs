500d9a79fed3256bae67730fc1518874f32c022b
Brad Weir <bradrweir@gmail.com>
2022-03-17 10:29:59 -0400

    dbcontext

---
+++ backend/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace template_csharp_album_collections
+{
+    public class ApplicationDbContext : DbContext
+    {
+        const string CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb; Database=AlbumsDB; Trusted_Connection=True";
+
+        //Add DBSets here
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(CONNECTION_STRING);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- backend/Startup.cs
+++ backend/Startup.cs
@@ -27,6 +27,25 @@ namespace template_csharp_album_collections
         {
 
             services.AddControllers();
+
+            //just creating these swagger calls for just in case
+            //I did not download the nuget packages
+            //services.AddSwaggerGen(c =>
+            //{
+            //  c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodosData", Version = "v1" });
+            //});
+
+            services.AddDbContext<ApplicationDbContext>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", services =>
+                {
+                    services.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +54,8 @@ namespace template_csharp_album_collections
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodosData v1"));
             }
 
             app.UseHttpsRedirection();
