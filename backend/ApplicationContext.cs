b848a8cbbecd58dc7ef62024a05b6514b2e7309d
Jessica Wang <jessicaw.wyt@gmail.com>
2022-03-18 11:22:32 -0400

    added Album Model

---
+++ backend/ApplicationContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using template_csharp_album_collections.Models;
+
+namespace template_csharp_album_collections
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        const string CONNECTION_STRING = "Server=(localdb)\\mssqllocaldb; Database=AlbumsDB; Trusted_Connection=True";
+
+        //Add DBSets here
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(CONNECTION_STRING);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Artist
+
+            //Album
+            modelBuilder.Entity<Album>().HasData(
+                new Album() { Id = 1,
+                    Title = "DAMN.",
+                    Image = "https://m.media-amazon.com/images/I/71FvMBLmheL._SL1200_.jpg",
+                    ArtistId = 1,
+                    RecordLabel = "Aftermath"}
+                );
+            modelBuilder.Entity<Album>().HasData(
+                new Album()
+                {
+                    Id = 2,
+                    Title = "Plastic Hearts",
+                    Image = "https://i.pinimg.com/originals/38/e0/6e/38e06ed03db63c6d75338a5a76b3135b.png",
+                    ArtistId = 2,
+                    RecordLabel = "RCA Records"
+
+                }
+                );
+            modelBuilder.Entity<Album>().HasData(
+                new Album()
+                {
+                    Id = 1,
+                    Title = "House of Balloons",
+                    Image = "https://m.media-amazon.com/images/I/81-blrK5yoL._SL1400_.jpg",
+                    ArtistId = 3,
+                    RecordLabel = "XO"
+                }
+                );
+            //Song
+
+            //Review
+
+
+
+
+        }
+    }
+}
---
+++ backend/Controllers/AlbumController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using template_csharp_album_collections.Models;
+
+namespace template_csharp_album_collections.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public AlbumController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Album> Get()
+        {
+            return _context.Albums.ToList();
+
+        }
+
+
+
+    }
+}
