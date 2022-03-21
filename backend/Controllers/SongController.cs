e77d81cb02ee6d098f005c84b8344c1ed724d8ec
Jessica Wang <jessicaw.wyt@gmail.com>
2022-03-21 15:11:20 -0400

    Added SongCrud

--- backend/Controllers/SongController.cs
+++ backend/Controllers/SongController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using template_csharp_album_collections.Models;
 
 namespace template_csharp_album_collections.Controllers
 {
@@ -14,6 +17,54 @@ namespace template_csharp_album_collections.Controllers
             _context = context; 
         }
 
-        
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return _context.Songs.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Song Get(int id)
+        {
+            return _context.Songs.Find(id);
+        }
+
+        [HttpPost]
+        public bool Post(Song song)
+        {
+            try
+            {
+                _context.Songs.Add(song);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        [HttpPut]
+        public bool Put(Song song)
+        {
+            try
+            {
+                _context.Songs.Update(song);
+                _context.SaveChanges(true);
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var songToDelete = _context.Songs.Find(id);
+            _context.Songs.Remove(songToDelete);
+            _context.SaveChanges();
+        }
     }
 }
