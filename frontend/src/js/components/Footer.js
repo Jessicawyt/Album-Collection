0e4c0c8d0da21390605ec686c7b1f50689a36a26
Denzale McIntyre <denzale.mcintyre@aol.com>
2022-03-24 15:40:44 -0400

    footer update

--- frontend/src/js/components/Footer.js
+++ frontend/src/js/components/Footer.js
@@ -6,10 +6,21 @@ export default {
 
 
 function SetupFooter(){
-    
+ 
 
     return `
         <div>
+        
+        
+        <section class="Links">
+       <ul><a href="https://github.com/BradRWeir" target="_blank">Link to Brad's Github</a> </ul>
+       <ul><a href="https://github.com/DenzaleM" target="_blank"> Denzale's Github Link</a> </ul>
+       <ul><a href="http://github.com/adia1bug"target="_blank" >Qadriyyah's Github Link </a> </ul>
+       <ul><a href="https://www.linkedin.com/in/jessica-yuting-wang/"target="_blank" >Jessica's Linken </a> </ul>
+       <ul><a href="https://www.linkedin.com/in/darius-hmmond/" target="_blank">Daruis Linken Link </a> </ul>
+       <ul><a href="https://www.linkedin.com/in/rimma-girsheva" target="_blank">Rimma's Linken Link</a> </ul>
+        </section>
+
             <p>&copy; 2022   </p>
         </div>
     `;
